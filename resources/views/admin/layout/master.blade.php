8bec50b145e6f28849f97d9a9f435c34868c5a23
herdianurdin <herdianurdin@gmail.com>
2022-05-16 05:52:13 +0700

    add custom script

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -46,6 +46,9 @@
   <script src="{{ asset('assets/js/scripts.js') }}"></script>
   <script src="{{ asset('assets/js/custom.js') }}"></script>
 
+  <!-- Custom JS Scripts -->
+  <script src="{{ asset('assets/js/page/bootstrap-modal.js') }}"></script>
+
 </body>
 
 </html>
